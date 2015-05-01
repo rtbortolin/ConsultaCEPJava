19fe1a18d8b170b86ab219b1dec8a790f41d954b
Rafael Bortolin <rt.bortolin@gmail.com>
2015-05-01 18:21:35 -0300

    Enabling http cache

--- src/main/java/consultaCEP/web/api/CepController.java
+++ src/main/java/consultaCEP/web/api/CepController.java
@@ -4,17 +4,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
-
-import org.apache.commons.lang3.time.StopWatch;
 
 import main.java.consultaCEP.domain.entities.Address;
 import main.java.consultaCEP.domain.services.CorreiosWebAccess;
 import main.java.consultaCEP.domain.services.SearchCepService;
 import main.java.consultaCEP.infra.db.AddressRepository;
 import main.java.consultaCEP.interfaces.ISearchCepService;
+
+import org.apache.commons.lang3.time.StopWatch;
 
 @Path("/cep")
 public class CepController {
@@ -35,10 +36,15 @@ public class CepController {
 	public Response getCep(@PathParam("cep") String cep) {
 		StopWatch sw = new StopWatch();
 		sw.start();
+				
 		Address address = service.getAddress(cep);
 
-		Response response = Response.status(200).entity(address).build();
-		Response corsResponse = createCorsResponse(response);
+		ResponseBuilder responseBuilder = Response.status(200).entity(address);
+		CacheControl cache = new CacheControl();
+		cache.setMaxAge(86400);
+		cache.setPrivate(true);
+		responseBuilder.cacheControl(cache);
+		Response corsResponse = createCorsResponse(responseBuilder.build());
 		
 		sw.stop();
 		System.out.println("Request time: " + cep + " | " + sw);
